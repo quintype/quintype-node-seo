06f19ca4707b0f1e1b3b8c82d7a4bac4da6864c7
Tejas Dinkar <tejas@gja.in>
2017-12-08 13:17:11 +0530

    Fixing tests

--- test/seo_test.js
+++ test/seo_test.js
@@ -2,45 +2,39 @@ const {SEO, StaticTags} = require("..");
 
 var assert = require('assert');
 
-function runTest(seoConfig, config, pageType, data) {
+function getSeoMetadata(seoConfig, config, pageType, data) {
   return new SEO(seoConfig)
     .getMetaTags(config, pageType, data)
-    .then(tags => tags.toString());
+    .toString();
 }
 
 describe('SEO', function() {
   describe('StaticTags', function() {
-    it('should return a list of static tags', function(done) {
+    it('should return a list of static tags', function() {
       const seoConfig = {
         generators: [StaticTags],
         staticTags: { "twitter:site": "@Quintype_inc" }
       };
-      runTest(seoConfig, {}, 'home-page', {})
-        .then(string => assert.equal('<meta name="twitter:site" content="@Quintype_inc"/>', string))
-        .then(done)
-        .catch(done);
+      const string = getSeoMetadata(seoConfig, {}, 'home-page', {});
+      assert.equal('<meta name="twitter:site" content="@Quintype_inc"/>', string);
     });
 
-    it('supports other twitter attributes', function(done) {
+    it('supports other twitter attributes', function() {
       const seoConfig = {
         generators: [StaticTags],
         staticTags: { "google-play-app": "my-play-app" }
       };
-      runTest(seoConfig, {}, 'home-page', {})
-        .then(string => assert.equal('<meta name="google-play-app" content="my-play-app"/>', string))
-        .then(done)
-        .catch(done);
+      const string = getSeoMetadata(seoConfig, {}, 'home-page', {});
+      assert.equal('<meta name="google-play-app" content="my-play-app"/>', string);
     });
 
-    it('converts og tags to meta property', function(done) {
+    it('converts og tags to meta property', function() {
       const seoConfig = {
         generators: [StaticTags],
         staticTags: { "og:site_name": "Quintype" }
       };
-      runTest(seoConfig, {}, 'home-page', {})
-        .then(string => assert.equal('<meta property="og:site_name" content="Quintype"/>', string))
-        .then(done)
-        .catch(done);
+      const string = getSeoMetadata(seoConfig, {}, 'home-page', {});
+      assert.equal('<meta property="og:site_name" content="Quintype"/>', string);
     })
   });
 });
