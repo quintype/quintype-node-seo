b5d2bef1a4d55ff194983039efa52e1f98df75c0
Jeevan Kishore <kishor080@gmail.com>
2018-04-19 14:01:56 +0530

    Implement article structured data

--- src/structured-data-tags.js
+++ src/structured-data-tags.js
@@ -10,26 +10,46 @@ function ldJson(type, fields) {
   };
 }
 
-export function StructuredDataTags({structuredData = {}}, config, pageType, data = {}, {url}) {
-  const tags = [];
-  const {story = {}, config: publisherConfig = {}} = data.data;
-  const {storyType = ''} = publisherConfig['publisher-settings'] || {};
+function generateArticleData (story = {}, publisherConfig = {}){
+  const metaKeywords = story.seo && story.seo['meta-keywords'] || [];
+  return {
+    "author": story['author-name'],
+    "editor": story['author-name'],
+    "keywords": metaKeywords,
+    "publisher": story['author-name'],
+    "url": `${publisherConfig['sketches-host']}/${story.slug}`,
+    "dateCreated": new Date(story['created-at']),
+    "dateModified": new Date(story['updated-at']),
+  }
+}
 
-
-  const articleStructureData = {
+function generateNewsArticleData (story = {}, publisherConfig = {}) {
+  return {
     'headline' : story.headline,
     "alternativeHeadline": story.subheadline,
     "image": [`${publisherConfig['cdn-image']}/${story['hero-image-s3-key']}`],
     "datePublished": new Date(story['published-at']),
     "description": story.summary,
   };
+}
+
+export function StructuredDataTags({structuredData = {}}, config, pageType, data = {}, {url}) {
+  const tags = [];
+  const {story = {}} = data.data;
+  const {config: publisherConfig = {}} = data;
+  const {articleType = ''} = publisherConfig['publisher-settings'] || {};
+
+  const articleData = generateArticleData(story, publisherConfig);
+
+  tags.push(ldJson("Article", articleData));
 
   if(structuredData.organization) {
     tags.push(ldJson("Organization", structuredData.organization));
   }
 
-  if(storyType === 'news-article') {
-    tags.push(ldJson('news-article', articleStructureData));
+  if(articleType === 'news-article') {
+    const newsArticleData = Object.assign({}, articleData, generateNewsArticleData(story, publisherConfig));
+    tags.push(ldJson('NewsArticle', newsArticleData));
   }
 
   // All Pages have: Publisher, Site
