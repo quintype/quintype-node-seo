81858dff45a6cd98c248bfdfe05ed0170e387ffc
Tejas Dinkar <tejas@gja.in>
2018-04-27 13:23:00 +0530

    Refactor the image tags code (no new functionality)

--- src/image-tags.js
+++ src/image-tags.js
@@ -1,26 +1,36 @@
 import {get, isEmpty} from 'lodash';
 import {FocusedImage} from 'quintype-js';
 
+function pickImageFromCard(story, cardId) {
+  const { metadata = {} } = story.cards.find(card => card.id === cardId) || {};
+  if(metadata && !isEmpty(metadata) && get(metadata, ['social-share', 'image', 'key'], false)) {
+    return new FocusedImage(metadata['social-share'].image.key, metadata['social-share'].image.metadata || {});
+  }
+}
+
+function pickImageFromStory(story) {
+  return new FocusedImage(story["hero-image-s3-key"], story["hero-image-metadata"] || {})
+}
+
+// The image is grabbed from the story, else from from the collection
+function pickImage(pageType, data, url) {
+  if(pageType == 'story-page' && url.query && url.query.cardId) {
+    const story = get(data, ['data', 'story']) || {};
+    return pickImageFromCard(story, url.query.cardId) || pickImageFromStory(story);
+  } else if(pageType == 'story-page') {
+    const story = get(data, ['data', 'story']) || {};
+    return pickImageFromStory(story);
+  }
+}
+
 export function ImageTags(seoConfig, config, pageType, data, {url = {}}) {
-  if(pageType != 'story-page')
-    return [];
+  const image = pickImage(pageType, data, url);
 
-  const story = get(data, ['data', 'story']) || {};
-
-  if(!story['hero-image-s3-key'])
+  if(!image) {
     return [];
+  }
 
   const tags = [];
-  let image = new FocusedImage(story["hero-image-s3-key"], story["hero-image-metadata"] || {});
-
-  if(url.query && url.query.cardId){
-      const { metadata = {} } = story.cards.find(card => card.id === url.query.cardId) || {};
-      if(metadata
-        && !isEmpty(metadata)
-        && get(metadata, ['social-share', 'image', 'key'], false)){
-          image = new FocusedImage(metadata['social-share'].image.key, metadata['social-share'].image.metadata || {});
-      }
-  }
 
   if(seoConfig.enableTwitterCards) {
     tags.push({name: "twitter:image", content: `https://${config['cdn-image']}/${image.path([16, 9], {w: 1200, auto: "format,compress"})}`})
@@ -29,7 +39,7 @@ export function ImageTags(seoConfig, config, pageType, data, {url = {}}) {
   if(seoConfig.enableOgTags) {
     tags.push({property: "og:image", content: `https://${config['cdn-image']}/${image.path([40, 21], {w: 1200, auto: "format,compress"})}`});
     tags.push({property: "og:image:width", content: 1200});
-    if(get(story, ["hero-image-metadata", "focus-point"])) {
+    if(get(data, ["data", "story", "hero-image-metadata", "focus-point"])) {
       tags.push({property: "og:image:height", content: 630})
     }
   }
