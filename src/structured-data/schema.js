8110c1216c869e16d070ecbdd44eb7fd7ac681e2
Garima Kamboj <garima.kamboj@icloud.com>
2018-09-12 10:56:05 +0530

    Fix slug passed in Blog posting schema

--- src/structured-data/schema.js
+++ src/structured-data/schema.js
@@ -18,7 +18,7 @@ export function getSchemaBlogPosting(card = {}, author = {}, headline = '', imag
   const {website : {url = ''} = {}} = structuredData;
   return Object.assign({},
     getSchemaType("BlogPosting"),
-    getSchemaMainEntityOfPage(`${url}${story.slug}`),
+    getSchemaMainEntityOfPage(`${url}/${story.slug}`),
     getSchemaPublisher(structuredData.organization),
     {
       "dateModified": new Date(card['card-updated-at']),
