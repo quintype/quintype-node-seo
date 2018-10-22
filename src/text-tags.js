13bc106320b72ff3231f7d6325fec219532f6813
Athira <athira@quintype.com>
2018-10-22 15:32:16 +0530

    added a fallback to home data

--- src/text-tags.js
+++ src/text-tags.js
@@ -42,18 +42,22 @@ function buildTagsFromStory(config, story, url = {}) {
   return storyMetaData;
 }
 
-function buildTagsFromTopic(config, data, url = {}) {
+function buildTagsFromTopic(config, tag, url = {}) {
+  console.log('tag', tag);
+  console.log('url', url);
+  if(!tag)
+    return;
 
-  const topicUrl = `${config['sketches-host']}/topic/${data.tag}`;
+  const topicUrl = `${config['sketches-host']}${url.pathname}`;
 
   const topicMetaData = {
-    "page-title":data.tagName,
-    description: data.tagDescription || data.tagName,
-    keywords: data.tagName,
+    "page-title":tag.name,
+    description: tag.description || tag.name,
+    keywords: tag.name,
     canonicalUrl: topicUrl,
     ogUrl: topicUrl,
-    ogTitle: data.tagName,
-    ogDescription: data.tagDescription || data.tagName
+    ogTitle: tag.name,
+    ogDescription: tag.tagDescription || tag.name
   };
 
   return topicMetaData;
@@ -94,7 +98,7 @@ function getSeoData(config, pageType, data, url = {}, seoConfig = {}) {
   switch(pageType) {
     case 'home-page': return findRelevantConfig(page => page['owner-type'] === 'home')
     case 'section-page': return findRelevantConfig(page => page['owner-type'] === 'section' && page['owner-id'] === get(data, ['data', 'section', 'id'])) || getSeoData(config, 'home-page', data, url);
-    case 'tag-page': return buildTagsFromTopic(config, get(data, ["data"]), url) || getSeoData(config, "home-page", data, url);
+    case 'tag-page': return buildTagsFromTopic(config, get(data, ["data", "tag"]), url) || getSeoData(config, "home-page", data, url);
     case 'story-page': return buildTagsFromStory(config, get(data, ["data", "story"]), url) || getSeoData(config, "home-page", data, url);
     default: return getSeoData(config, 'home-page', data, url);
   }
