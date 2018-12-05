869317927f4b94f32cd683ddb2bab64dd2917424
Garima Kamboj <garima.kamboj@icloud.com>
2018-12-05 14:15:48 +0530

    feat(author-page): Add support for custom author-page seo data (#25)
    * Add seo data for author page
    
    * Update author page seo data
    
    * Refactor
    
    * Update url in author seo

--- src/text-tags.js
+++ src/text-tags.js
@@ -60,6 +60,26 @@ function buildTagsFromTopic(config, tag, url = {}) {
   return topicMetaData;
 }
 
+function buildTagsFromAuthor(config, author, url = {}) {
+  if(isEmpty(author)) return;
+
+  const authorName = author.name;
+  const authorUrl = `${config['sketches-host']}${url.pathname}`;
+  const publisherName = config['publisher-name'];
+  const description = author.bio || `View all articles written by ${author.name} on ${publisherName}`;
+
+  return {
+    title: authorName,
+    "page-title": authorName,
+    description: description,
+    keywords: `${authorName},${publisherName}`,
+    canonicalUrl: authorUrl,
+    ogUrl: authorUrl,
+    ogTitle: authorName,
+    ogDescription: description,
+  };
+}
+
 function buildCustomTags(customTags = {}, pageType = ''){
   const configObject = customTags[pageType];
   if(configObject) {
@@ -97,6 +117,7 @@ function getSeoData(config, pageType, data, url = {}, seoConfig = {}) {
     case 'section-page': return findRelevantConfig(page => page['owner-type'] === 'section' && page['owner-id'] === get(data, ['data', 'section', 'id'])) || getSeoData(config, 'home-page', data, url);
     case 'tag-page': return buildTagsFromTopic(config, get(data, ["data", "tag"]), url) || getSeoData(config, "home-page", data, url);
     case 'story-page': return buildTagsFromStory(config, get(data, ["data", "story"]), url) || getSeoData(config, "home-page", data, url);
+    case 'author-page': return buildTagsFromAuthor(config, get(data, ["data", "author"], {}), url) || getSeoData(config, "home-page", data, url);
     default: return getSeoData(config, 'home-page', data, url);
   }
 }
