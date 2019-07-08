ce3d421eb8973feeed7d67de2daa750fb8ffbe18
Tejas Dinkar <tejas@gja.in>
2019-07-08 15:06:07 +0530

    Feedback from Sinu

--- src/amp-tags.js
+++ src/amp-tags.js
@@ -1,23 +1,31 @@
 import get from 'lodash/get';
 import { isStoryPublic } from './utils';
 
-function storyPageAmpTags(story, ampStoryConfig) {
-  if(ampStoryConfig === 'public' && !isStoryPublic(story)) {
-    return [];
+function showAmpTag({ampStoryPages = true}, pageType, story) {
+  if(!ampStoryPages || pageType !== 'story-page') {
+    return false;
   }
 
-  return [{
-    tag: 'link',
-    rel: 'amphtml',
-    href: `/amp/story/${encodeURIComponent(story.slug)}`
-  }];
+  if (!get(story, ["is-amp-supported"])) {
+    return false;
+  }
+
+  if(ampStoryPages === 'public' && !isStoryPublic(story)) {
+    return false;
+  }
+
+  return true;
 }
 
 export function StoryAmpTags(seoConfig, config, pageType, data, opts) {
-  const ampStoryPages = get(seoConfig, ["ampStoryPages"], true);
-
-  if (ampStoryPages && pageType == 'story-page' && get(data, ["data", "story", "is-amp-supported"]))
-    return storyPageAmpTags(get(data, ["data", "story"]), ampStoryPages)
-  else
-    return []
+  const story = get(data, ["data", "story"], {});
+  if(showAmpTag(seoConfig, pageType, story)) {
+    return [{
+      tag: 'link',
+      rel: 'amphtml',
+      href: `/amp/story/${encodeURIComponent(story.slug)}`
+    }];
+  } else {
+    return [];
+  }
 }
