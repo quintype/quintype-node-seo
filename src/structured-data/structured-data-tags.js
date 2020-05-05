ec11905d9bceaa9fad3c809f7305fe4f6a76ae01
Anbu Surendhar <anbusurendhar.v@vikatan.com>
2020-05-05 12:48:05 +0530

    Changed getCustomPageCrumbs to getBreadCrumbs

--- src/structured-data/structured-data-tags.js
+++ src/structured-data/structured-data-tags.js
@@ -218,13 +218,14 @@ function generateBreadcrumbListData(pageType = "", publisherConfig = {}, data = 
     return sectionCrumbsDataList;
   }
 
-  function getCustomPageCrumbs(collection = {}, breadcrumb = {}) {
+  function getBreadCrumbs(breadcrumb = {}) {
     const crumbsDataList = [{ url: breadcrumb.url, name: breadcrumb.name}];
-    return addCrumb(crumbsDataList, collection);
+    return addCrumb(crumbsDataList);
   }
 
   if(data.breadcrumbs && ("name" in data.breadcrumbs) && ("url" in data.breadcrumbs)) {
-    breadcrumbsDataList = breadcrumbsDataList.concat(getCustomPageCrumbs(data.collection, data.breadcrumbs));
+    breadcrumbsDataList = breadcrumbsDataList.concat(getBreadCrumbs(data.breadcrumbs));
+    return getSchemaBreadcrumbList(breadcrumbsDataList);
   }
   switch (pageType) {
     case "section-page": breadcrumbsDataList = breadcrumbsDataList.concat(getSectionPageCrumbs(data.section)); break;
