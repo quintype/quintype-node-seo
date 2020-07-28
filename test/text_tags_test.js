a26775a7fe573c4a3397a418d0756c568b185640
Deokumarjnu <deo.kumar.jnu@gmail.com>
2020-07-28 12:25:31 +0530

    added test cases for customseo title

--- test/text_tags_test.js
+++ test/text_tags_test.js
@@ -475,6 +475,18 @@ describe('TextTags', function() {
       assert.equal("My Title", seo.getTitle(config, 'home-page', {title: "My Title"}, {}));
       assert.equal("My Title", seo.getTitle(config, 'home-page', {data: {title: "My Title"}}, {}));
     });
+
+    it("can also get the customSeo title if passed in from data", function() {
+      const seo = new SEO({generators: []});
+      const config = {"seo-metadata": [{"owner-type": 'home', "owner-id": null, "data": {'page-title': "Foobar"}}]};
+      assert.equal("My Customseo Title", seo.getTitle(config, 'home-page', {data: {customSeo: {title: "My Customseo Title"}}}, {}));
+    });
+
+    it("fallback to default seo title if the customSeo title not passed from data", function() {
+      const seo = new SEO({generators: []});
+      const config = {"seo-metadata": [{"owner-type": 'home', "owner-id": null, "data": {'page-title': "Foobar"}}]};
+      assert.equal("My Title", seo.getTitle(config, 'home-page', {data: {customSeo: {title: ""}, title: "My Title"}}, {}));
+    });
   });
 
   describe('Tag Page', function() {
