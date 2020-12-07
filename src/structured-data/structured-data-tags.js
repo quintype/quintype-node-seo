f2be4b271dd5e495672d1beb3908dfb5743c9ee9
Shraddha Kesari <shraddha.kesari@gmail.com>
2020-12-07 16:13:28 +0530

    fix(isPartof schema): Duplicate productID issue (#330)
    * Fixes duplicate productID issue
    
    * chore(release): 1.38.26-amp-subcriptions-issue.0
    
    * Makes code DRYer
    
    * chore(release): 1.38.26-amp-subcriptions-issue.1
    
    * Resets version with master

--- src/structured-data/structured-data-tags.js
+++ src/structured-data/structured-data-tags.js
@@ -158,7 +158,7 @@ function generateArticleData(
         pageType
       ),
       isAccessibleForFree: storyAccessType,
-      isPartOf: generateIsPartOfData(story, publisherConfig, pageType),
+      isPartOf: generateIsPartOfDataForArticle(story, publisherConfig, pageType),
     },
     articleSectionObj(story)
   );
@@ -192,25 +192,8 @@ function storyAccess(access) {
   }
 }
 
-function generateIsPartOfData(story = {}, publisherConfig = {}, pageType = '') {
-  const publisherName = publisherConfig['publisher-name'];
-  const productId = publisherConfig['publisher-name'] + '.com:basic';
-  return pageType === 'story-page-amp'
-    ? Object.assign(
-        {},
-        {
-          '@type': ['CreativeWork', 'WebPage', 'Product'],
-          url: `${publisherConfig['sketches-host']}/${story.slug}`,
-          name: publisherName,
-          productID: productId,
-          primaryImageOfPage: generateArticleImageData(
-            story['hero-image-s3-key'],
-            publisherConfig,
-            pageType
-          ),
-        }
-      )
-    : Object.assign(
+function generateIsPartOfDataForArticle(story = {}, publisherConfig = {}, pageType = '') {
+  return Object.assign(
         {},
         {
           '@type': 'WebPage',
@@ -222,6 +205,30 @@ function generateIsPartOfData(story = {}, publisherConfig = {}, pageType = '') {
           ),
         }
       );
+}
+
+function generateIsPartOfDataForNewsArticle(story ={}, publisherConfig = {}, pageType = '', structuredData = {}) {
+  const publisherName = publisherConfig['publisher-name'];
+  const productId = publisherConfig['publisher-name'] + '.com:basic';
+  const isPartOfData = generateIsPartOfDataForArticle(story, publisherConfig, pageType);
+
+  if(pageType === 'story-page-amp' && structuredData.isAmpSubscriptionsEnabled) {
+    return Object.assign(
+      {},
+      {
+        '@type': ['WebPage', 'CreativeWork', 'Product'],
+        name: publisherName,
+        productID: productId,
+        url: `${publisherConfig['sketches-host']}/${story.slug}`,
+        primaryImageOfPage: generateArticleImageData(
+          story['hero-image-s3-key'],
+          publisherConfig,
+          pageType
+        ),
+      }
+    )
+  }
+  return isPartOfData
 }
 
 function generateHasPartData(storyAccess) {
@@ -255,7 +262,7 @@ function generateNewsArticleData(
           : '',
       description: story.summary,
       isAccessibleForFree: storyAccessType,
-      isPartOf: generateIsPartOfData(story, publisherConfig, pageType),
+      isPartOf: generateIsPartOfDataForNewsArticle(story, publisherConfig, pageType, structuredData),
     },
     generateHasPartData(storyAccessType)
   );
