ff63b43a263c84550b84553ba3b85aabc31652d1
Harshith46 <45616022+Harshith46@users.noreply.github.com>
2021-06-16 15:35:02 +0530

    Update image-tags.js

--- src/image-tags.js
+++ src/image-tags.js
@@ -1,54 +1,56 @@
-import { get, isEmpty } from "lodash";
-import { FocusedImage } from "quintype-js";
+import {get, isEmpty} from 'lodash';
+import {FocusedImage} from 'quintype-js';
 
 function pickImageFromCard(story, cardId) {
-  const { metadata = {} } = story.cards.find((card) => card.id === cardId) || {};
-  if (metadata && !isEmpty(metadata) && get(metadata, ["social-share", "image", "key"], false)) {
-    return new FocusedImage(metadata["social-share"].image.key, metadata["social-share"].image.metadata || {});
+  const { metadata = {} } = story.cards.find(card => card.id === cardId) || {};
+  if(metadata && !isEmpty(metadata) && get(metadata, ['social-share', 'image', 'key'], false)) {
+    return new FocusedImage(metadata['social-share'].image.key, metadata['social-share'].image.metadata || {});
   }
 }
 
 function pickImageFromStory(story) {
-  function getAlternateProperties(type, key) {
-    return get(story, ["alternative", `${type}`, "default", "hero-image", `${key}`]);
-  }
 
-  const alternateSocialMetadata = getAlternateProperties("social", "hero-image-metadata");
-  const alternateHomeMetadata = getAlternateProperties("home", "hero-image-metadata");
-  const alternateHomeS3Key = getAlternateProperties("home", "hero-image-s3-key");
-  const alternateSocialS3Key = getAlternateProperties("social", "hero-image-s3-key");
-
-  const socialAlternateHeroImageS3Metadata =
-    alternateSocialMetadata || alternateHomeMetadata || story["hero-image-metadata"];
-
-  const socialAlternateHeroImageS3Key = alternateSocialS3Key || alternateHomeS3Key || story["hero-image-s3-key"];
-
-  return new FocusedImage(socialAlternateHeroImageS3Key, socialAlternateHeroImageS3Metadata || {});
+  function getAlternateProperties (type, key) {
+    return get(story, ["alternative", `${type}`, "default", "hero-image", `${key}`]) ;
+   }
+ 
+   const alternateSocialMetadata = getAlternateProperties("social", "hero-image-metadata");
+   const alternateHomeMetadata = getAlternateProperties("home", "hero-image-metadata");
+   const alternateHomeS3Key = getAlternateProperties("home", "hero-image-s3-key");
+   const alternateSocialS3Key = getAlternateProperties("social", "hero-image-s3-key");
+ 
+ 
+   const socialAlternateHeroImageS3Metadata = (alternateSocialMetadata ? alternateSocialMetadata : alternateHomeMetadata)  ||  story["hero-image-metadata"];
+ 
+   const socialAlternateHeroImageS3Key = (alternateSocialS3Key ? alternateSocialS3Key : alternateHomeS3Key) || story["hero-image-s3-key"];
+ 
+   return new FocusedImage(socialAlternateHeroImageS3Key, socialAlternateHeroImageS3Metadata || {});
 }
 
 function pickImageFromCollection(collection) {
   const coverImage = get(collection, ["metadata", "cover-image"]) || {};
-  if (!coverImage["cover-image-s3-key"]) return;
+  if(!coverImage["cover-image-s3-key"])
+    return;
 
-  return new FocusedImage(coverImage["cover-image-s3-key"], coverImage["cover-image-metadata"] || {});
+  return new FocusedImage(coverImage["cover-image-s3-key"], coverImage["cover-image-metadata"] || {})
 }
 
 // The image is grabbed from the story, else from from the collection
 function pickImage(pageType, data, url) {
-  if (pageType === "story-page" && url.query && url.query.cardId) {
-    const story = get(data, ["data", "story"]) || {};
+  if(pageType === 'story-page' && url.query && url.query.cardId) {
+    const story = get(data, ['data', 'story']) || {};
     return pickImageFromCard(story, url.query.cardId) || pickImageFromStory(story);
-  } else if (pageType === "visual-story" && url.query && url.query.cardId) {
-    const story = get(data, ["story"]) || {};
+  }else if(pageType === 'visual-story' && url.query && url.query.cardId) {
+    const story = get(data, ['story']) || {};
     return pickImageFromCard(story, url.query.cardId) || pickImageFromStory(story);
-  } else if (pageType === "story-page" || pageType === "story-page-amp") {
-    const story = get(data, ["data", "story"]) || {};
+  } else if(pageType === 'story-page' || pageType === 'story-page-amp') {
+    const story = get(data, ['data', 'story']) || {};
     return pickImageFromStory(story);
-  } else if (pageType === "visual-story") {
-    const story = get(data, ["story"]) || {};
+  } else if(pageType === 'visual-story') {
+    const story = get(data, ['story']) || {};
     return pickImageFromStory(story);
-  } else if (get(data, ["data", "collection"])) {
-    return pickImageFromCollection(get(data, ["data", "collection"]));
+  } else if(get(data, ['data', 'collection'])) {
+    return pickImageFromCollection(get(data, ['data', 'collection']))
   }
 }
 
@@ -65,38 +67,24 @@ function pickImage(pageType, data, url) {
  * @param {boolean} seoConfig.enableTwitterCards Add twitter tags
  * @param {...*} params See {@link Generator} for other Parameters
  */
-export function ImageTags(seoConfig, config, pageType, data, { url = {} }) {
+export function ImageTags(seoConfig, config, pageType, data, {url = {}}) {
   const image = pickImage(pageType, data, url);
 
-  if (!image) {
+  if(!image) {
     return [];
   }
 
   const tags = [];
 
-  if (seoConfig.enableTwitterCards) {
-    tags.push({
-      name: "twitter:image",
-      content: `https://${config["cdn-image"]}/${image.path([16, 9], {
-        w: 1200,
-        auto: "format,compress",
-        ogImage: true,
-      })}`,
-    });
+  if(seoConfig.enableTwitterCards) {
+    tags.push({name: "twitter:image", content: `https://${config['cdn-image']}/${image.path([16, 9], {w: 1200, auto: "format,compress", ogImage: true})}`})
   }
 
-  if (seoConfig.enableOgTags) {
-    tags.push({
-      property: "og:image",
-      content: `https://${config["cdn-image"]}/${image.path([40, 21], {
-        w: 1200,
-        auto: "format,compress",
-        ogImage: true,
-      })}`,
-    });
-    tags.push({ property: "og:image:width", content: 1200 });
-    if (get(image, ["metadata", "focus-point"])) {
-      tags.push({ property: "og:image:height", content: 630 });
+  if(seoConfig.enableOgTags) {
+    tags.push({property: "og:image", content: `https://${config['cdn-image']}/${image.path([40, 21], {w: 1200, auto: "format,compress", ogImage: true})}`});
+    tags.push({property: "og:image:width", content: 1200});
+    if(get(image, ["metadata", "focus-point"])) {
+      tags.push({property: "og:image:height", content: 630})
     }
   }
 
